6d476ffa413a96d9de23f70393e4036ba0851468
le.pham <phamle21@gmail.com>
2023-10-07 16:38:53 +0700

    Revert "build filament"
    This reverts commit 59ffa911ba99cdc5bc5b2386d4ea17ee40b6ef9e.

--- boardinghouse_backend/database/seeders/DatabaseSeeder.php
+++ boardinghouse_backend/database/seeders/DatabaseSeeder.php
@@ -3,10 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,13 +18,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        User::insert([
-            'user_name' => 'phamle21',
-            'email' => 'phamle21@gmail.com',
-            'phone' => '091649826',
-            'password' => Hash::make('phamle21'),
-            'name' => 'Phạm Lê',
-        ]);
     }
 }
