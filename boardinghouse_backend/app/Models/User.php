06b3cedf38232120cede1dc0c1d9eafbcb0d4782
GiaBao202 <nbao7757@gmail.com>
2023-10-08 23:47:25 +0700

    update modle

--- boardinghouse_backend/app/Models/User.php
+++ boardinghouse_backend/app/Models/User.php
@@ -29,9 +29,7 @@ class User extends Authenticatable
         'gender',
         'address',
         'birthday',
-        'email_verified_at',
-        'created_at',
-        'updated_at'
+        'email_verified_at'
     ];
 
     /**
